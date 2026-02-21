bb0394654a3596fc7532d590580feca245b736ce
Andrew Shchelykalnov <shchav@mail.ru>
2026-02-21 20:15:16 +0300

    Lesson21

--- dist/js/main.js
+++ dist/js/main.js
@@ -15,7 +15,7 @@
   \**********************/
 (__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
 
-eval("{\nconst timer = __webpack_require__(/*! ./modules/timer */ \"./src/modules/timer.js\");\nconst menu = __webpack_require__(/*! ./modules/menu.js */ \"./src/modules/menu.js\");\nconst modal = __webpack_require__(/*! ./modules/modal.js */ \"./src/modules/modal.js\");\n\ntimer('23 february 2026 12:40:00');\nmenu();\nmodal();\n\n//# sourceURL=webpack://3dglo/./src/index.js?\n}");
+eval("{\nconst timer = __webpack_require__(/*! ./modules/timer */ \"./src/modules/timer.js\");\nconst menu = __webpack_require__(/*! ./modules/menu.js */ \"./src/modules/menu.js\");\nconst modal = __webpack_require__(/*! ./modules/modal.js */ \"./src/modules/modal.js\");\nconst validators = __webpack_require__(/*! ./modules/validators.js */ \"./src/modules/validators.js\");\n\ntimer('23 february 2026 12:40:00');\nmenu();\nmodal();\nvalidators();\n\n//# sourceURL=webpack://3dglo/./src/index.js?\n}");
 
 /***/ },
 
@@ -46,6 +46,16 @@ eval("{const modal = () => {\r\n\r\n    const modal = document.querySelector('.p
 (module) {
 
 eval("{\nconst timer = (deadline) => {\n    const timerHours = document.getElementById('timer-hours');\n    const timerMinutes = document.getElementById('timer-minutes');\n    const timerSeconds = document.getElementById('timer-seconds');\n\n    let idInterval;\n\n    const getTimeRemaining = () => {\n        let dateStop = new Date(deadline).getTime();\n        let dateNow = new Date().getTime();\n        let timeRemaining = (dateStop - dateNow) / 1000;\n        let hours = Math.floor((timeRemaining / 60 / 60));\n        let minutes = Math.floor((timeRemaining / 60) % 60);\n        let seconds = Math.floor(timeRemaining % 60);\n\n        if (timeRemaining <= 1) {\n            clearInterval(idInterval);\n            return { timeRemaining: 0, hours: 0, minutes: 0, seconds: 0 }\n        }\n        return { timeRemaining, hours, minutes, seconds }\n    }\n\n    const addZero = (num) => {\n        let strNum = String(num);\n        if (strNum.length < 2) {\n            strNum = `0${strNum}`;\n        }\n        return strNum;\n    }\n\n    const updateClock = () => {\n        let getTime = getTimeRemaining();\n\n        timerHours.textContent = addZero(getTime.hours);\n        timerMinutes.textContent = addZero(getTime.minutes);\n        timerSeconds.textContent = addZero(getTime.seconds);\n    }\n\n    idInterval = setInterval(updateClock, 1000);\n}\n\nmodule.exports = timer;\n\n//# sourceURL=webpack://3dglo/./src/modules/timer.js?\n}");
+
+/***/ },
+
+/***/ "./src/modules/validators.js"
+/*!***********************************!*\
+  !*** ./src/modules/validators.js ***!
+  \***********************************/
+(module) {
+
+eval("{const validators = () => {\r\n\r\n    const allCalcInput = document.querySelectorAll(\r\n        '.calc-count, .calc-day, .calc-square')\r\n    allCalcInput.forEach((calcInput) => {\r\n        calcInput.addEventListener('input', (e) => {\r\n            e.target.value = e.target.value.replace(/\\D+/, '');\r\n        })\r\n    });\r\n\r\n    const allInputText = document.querySelectorAll(\r\n        'input[type=text]:not(.calc-count):not(.calc-day):not(.calc-square), \\\r\n        [placeholder=\"Ваше сообщение\"]'\r\n    )\r\n    allInputText.forEach((inputText) => {\r\n        inputText.addEventListener('input', (e) => {\r\n            e.target.value = e.target.value.replace(/[^-a-zA-Z ]/, '');\r\n        })\r\n    });\r\n\r\n    const allEmail = document.querySelectorAll(\r\n        'input[type=email]'\r\n    )\r\n    allEmail.forEach((email) => {\r\n        email.addEventListener('input', (e) => {\r\n            e.target.value = e.target.value.replace(/[^a-zA-Z0-9@-_.!~*\\']/, '');\r\n        })\r\n    });\r\n\r\n    const allTel = document.querySelectorAll(\r\n        'input[type=tel]'\r\n    )\r\n    allTel.forEach((tel) => {\r\n        tel.addEventListener('input', (e) => {\r\n            e.target.value = e.target.value.replace(/[^0-9()-]/, '');\r\n        })\r\n    });\r\n}\r\n\r\nmodule.exports = validators;\n\n//# sourceURL=webpack://3dglo/./src/modules/validators.js?\n}");
 
 /***/ }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 const timer = require('./modules/timer');
 const menu = require('./modules/menu.js');
 const modal = require('./modules/modal.js');
+const validators = require('./modules/validators.js');
 
 timer('23 february 2026 12:40:00');
 menu();
 modal();
+validators();
