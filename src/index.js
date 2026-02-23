eab2786a0179fb0f5cf524f29101439b3129d38d
Andrew Shchelykalnov <shchav@mail.ru>
2026-02-23 20:05:55 +0300

    Lesson23

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ const menu = require('./modules/menu.js');
 const modal = require('./modules/modal.js');
 const validators = require('./modules/validators.js');
 const tabs = require('./modules/tabs.js');
+const slider = require('./modules/slider.js');
 
 timer('27 february 2026 12:40:00');
 menu();
 modal();
 validators();
 tabs();
+slider();
